f8bfc44a72aa557235e65a6c745fa2d2460e3737
GV-watson <watsonw@mail.gvsu.edu>
2025-10-21 03:26:59 -0400

    Add files via upload
    Both the syrup and cream change to correct colors, must fix the no syrup option to match base.color

---
+++ src/Syrup.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="syrup" :style="{ backgroundColor: isSyruped.color }"></div>
+</template>
+
+<script setup lang="ts">
+import type { SyrupType } from "./stores/beverage";
+
+const props = defineProps<{ isSyruped: SyrupType }>();
+
+</script>
+<style lang="scss" scoped>
+.syrup {
+  transform: translateY(400%);
+  //background-color: #c6c6c6;
+  position: relative;
+  width: 100%;
+  height: 20%;
+  animation: pour-tea 2s 1s forwards;
+  z-index: 2;
+}
+</style>
